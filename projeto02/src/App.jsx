945cef04d9c9992b0962eb8d80db24526fb20a16
kauã oraboni <70423924+Orabonii@users.noreply.github.com>
2024-05-19 17:52:08 -0300

    Add files via upload

---
+++ projeto02/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'; 
+
+import Contador from './Components/Projeto02/Projeto02'; 
+import './App.css'
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <Contador /> 
+
+    </div> 
+
+  ); 
+
+} 
+
+  
+
+export default App; 
